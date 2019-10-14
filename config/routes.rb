cd013670deae1bb5345103f7ab9881bc0b1e9467
lemanh <le.duc.manhb@sun-asterisk.com>
2019-10-14 13:52:44 +0700

    init model user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< c65d84319136b527064b62d2ee7800b69e046ace
   scope "(:locale)", locale: /en/ do
     root to: "static_pages#home"
     get "static_pages/help"
     devise_for :users
   end
+=======
+  root to: "static_pages#home"
+  get "static_pages/help"
+  devise_for :users
+>>>>>>> init model user
 end
