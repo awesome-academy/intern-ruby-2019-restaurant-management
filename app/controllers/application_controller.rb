f7fa71332065051ceab60170b5392683ef5a1a1c
lemanh <le.duc.manhb@sun-asterisk.com>
2019-10-15 11:09:07 +0700

    sign up user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,15 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:email, :name, :phone, :password, :password_confirmation, :remember_me, :role]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< c65d84319136b527064b62d2ee7800b69e046ace
   scope "(:locale)", locale: /en/ do
     root to: "static_pages#home"
     get "static_pages/help"
-    devise_for :users
+    devise_for :users, controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations"
+    }
+    devise_scope :user do
+      get "sign_up", to: "users/registrations#new"
+      post "sign_up", to: "users/registrations#create"
+    end
   end
-=======
-  root to: "static_pages#home"
-  get "static_pages/help"
-  devise_for :users
->>>>>>> init model user
 end
